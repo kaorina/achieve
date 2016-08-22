6a002f23b0973a0b6169ad0e46fa9723184d065c
kaorina <zonorina@gmail.com>
2016-08-22 01:33:49 +0000

    Installed and add Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
+  
+  # before_actionでdeviseのストロングパラメーターにnameカラムを追加するメソッドを実行します。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  PERMISSIBLE_ATTRIBUTES = %i(name)
+  
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
+      devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+    end
 end
